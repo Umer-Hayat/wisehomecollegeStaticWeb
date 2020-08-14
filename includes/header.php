80600734076f19bb5e100931aeb942ed6ba432ab
umer33 <umerhayat520@gmail.com>
2020-08-14 23:19:08 +0500

    master

--- includes/header.php
+++ includes/header.php
@@ -95,7 +95,7 @@
                     <a class="nav-link" href="register.html">Register</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="login.html">Login</a>
+                    <a class="nav-link" href="login.html" data-toggle="modal" data-target="#myModal">Login</a>
                   </li>
                 </ul>
               </div>
@@ -104,3 +104,45 @@
         </div>
       </div>
     </header>
+
+   <!--  <button type="button" class="btn btn-primary" >
+    Open modal
+  </button>
+ -->
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">LogIn </h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <form method="post">
+          
+        
+        <!-- Modal body -->
+        <div class="container">
+          <div class="modal-body">
+          
+            <div class="form-group">
+              <label for="email" class="col-form-label">Email/Phone No:</label>
+              <input type="text" class="form-control" name="email" id="email">
+            </div>
+            <div class="form-group">
+              <label for="pass" class="col-form-label">Password:</label>
+              <input type="password" class="form-control" name="password" id="pass">
+            </div>
+          </div>
+          
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary" data-dismiss="modal">LogIn</button>
+          </div>
+          </div>
+        </form>
+        
+      </div>
+    </div>
+  </div>
