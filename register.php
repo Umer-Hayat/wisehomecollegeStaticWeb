823402854cc405051236fab0826ac364a46ffa95
umer33 <umerhayat520@gmail.com>
2020-09-27 09:03:49 +0500

    sending email

--- register.php
+++ register.php
@@ -15,7 +15,42 @@ include_once 'admin/classes/StudentManagement.php';
     $qualif = $_POST['qualif'];
     $q_from = $_POST['from'];
 
-    $check = $st->register($name,$fname,$cnic,$exp_date,$email,$password,$contact,$address,$qualif,$q_from);
+    // $check = $st->register($name,$fname,$cnic,$exp_date,$email,$password,$contact,$address,$qualif,$q_from);
+
+    // PHP Mailer
+
+    require 'PHPMailerAutoload.php';
+    require 'credential.php';
+
+    $mail = new PHPMailer;
+
+    // $mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = EMAIL;                 // SMTP username
+    $mail->Password = PASS;                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    $mail->setFrom(EMAIL, 'Info');
+    $mail->addAddress('umerhayat520@gmail.com');
+    $mail->addReplyTo(EMAIL);
+
+    // $mail->addAttachment('/var/tmp/file.tar.gz'); 
+    $mail->isHTML(true);                                  // Set email format to HTML
+
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    if(!$mail->send()) {
+        // echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        // echo 'Message has been sent';
+    }
     }
 ?>
 <br>
