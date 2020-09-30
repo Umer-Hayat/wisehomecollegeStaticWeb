3fa8a41a2c892ce01b6a4edcb72bcbb9ea067866
umer33 <umerhayat520@gmail.com>
2020-09-30 13:46:40 +0500

    master

--- admin/students.php
+++ admin/students.php
@@ -30,6 +30,89 @@ if(isset($_GET['del']))
         <br />
         <br />
         <div class="container-fluid">
+
+          <?php
+          if (isset($_GET['add'])) {
+
+
+          if(isset($_POST['submit'])){
+            $name = $_POST['name'];
+            $fname = $_POST['fname'];
+            $cnic = $_POST['cnic'];
+            $exp_date = $_POST['expdate'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $contact = $_POST['contact'];
+            $address = $_POST['address'];
+            $qualif = $_POST['qualif'];
+            $q_from = $_POST['from'];
+            $check = $st->register($name,$fname,$cnic,$exp_date,$email,$password,$contact,$address,$qualif,$q_from);
+            if ($check == "Data Inserted") {
+                echo '<script>window.location.replace("students.php")</script>';
+            }
+            }
+
+           ?>
+            <div class="row">
+            <!-- column -->
+            <div class="col-12">
+              <div class="card">
+                            <div class="card-body">
+                                <h3 class="card-title text-center">Add New Student</h3>
+                               
+                                <form class="form-material m-t-20 row" method="post">
+                                  <div class="row text-center">
+                                       <div style="color:red; margin-left: 20px; font-size:16px;"><?php
+                                           if (isset($_POST['submit'])) {
+                                               echo "$check";
+                                           }
+                                           ?>
+                                    </div>
+                                   </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="text" name="name" class="form-control form-control-line" placeholder="Enter Student Name"> 
+                                    </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="text" name="fname" class="form-control form-control-line" placeholder="Enter Student Father Name"> 
+                                    </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="number" name="cnic" class="form-control" placeholder="Enter CNIC Number">
+                                    </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="date" name="expdate" class="form-control" placeholder="Enter CNIC Number">
+                                    </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="email" name="email" class="form-control" placeholder="Enter Student Email">
+                                    </div>
+                                    <div class="form-group col-md-6 m-t-10">
+                                        <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                                    </div>
+                                    <div class="form-group col-md-4 m-t-10">
+                                        <input type="number" name="contact" class="form-control" placeholder="Enter Contact No">
+                                    </div>
+                                    <div class="form-group col-md-4 m-t-10">
+                                        <input type="text" name="qualif" class="form-control" placeholder="Enter Qualification">
+                                    </div>
+
+                                    <div class="form-group col-md-4 m-t-10">
+                                        <input type="text" name="from" class="form-control" placeholder="From">
+                                    </div>
+                                    <div class="form-group col-md-12 m-t-10">
+                                        <label>Address</label>
+                                        <textarea class="form-control" name="address" rows="2"></textarea>
+                                    </div>
+                                    <div class="col-md-12 m-t-10 text-center">
+                                        <input type="reset" class="btn btn-secondary" >
+                                        <input type="submit" name="submit" class="btn btn-primary" >
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+            </div>
+          </div>
+          <?php }
+           ?>
+
           <div class="row">
             <!-- column -->
 
@@ -37,7 +120,7 @@ if(isset($_GET['del']))
               <div class="card">
                 <div class="card-body">
                   <h4 class="card-title">Students</h4>
-                  <a href="addStudent.php" class="btn btn-primary">Add New Student</a>
+                  <a href="students.php?add" class="btn btn-primary">Add New Student</a>
                     <br>
                   <hr />
                   <div class="table-responsive m-t-10">
